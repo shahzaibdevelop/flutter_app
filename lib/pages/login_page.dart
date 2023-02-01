fdf8bbdd57b0cc4d97fbde21a97107890d1b6643
Shahzaib Hassan <shahzaibdevelop@gmail.com>
2023-02-01 16:53:27 -0100

    Animated Container and Inkwell Widget added

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:practice_app/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool changeButton = false;
+  String name = "";
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -27,11 +34,24 @@ class LoginPage extends StatelessWidget {
             SizedBox(
               height: 20,
             ),
+            Text(
+              "$name",
+              style: TextStyle(
+                fontSize: 28.0,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
               child: Column(
                 children: [
                   TextFormField(
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
                     decoration: InputDecoration(
                       labelText: "Username",
                       hintText: "Enter Username",
@@ -42,28 +62,44 @@ class LoginPage extends StatelessWidget {
                     decoration: InputDecoration(
                       labelText: "Password",
                       hintText: "Enter Password",
-                      labelStyle: TextStyle(
-                        color: Colors.purple[300],
-                      ),
                     ),
                   ),
                   SizedBox(
                     height: 40,
                   ),
-                  ElevatedButton(
-                    onPressed: () {
+                  InkWell(
+                    child: AnimatedContainer(
+                      duration: Duration(milliseconds: 700),
+                      width: changeButton ? 60.0 : 120,
+                      height: 60.0,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        borderRadius: changeButton
+                            ? BorderRadius.circular(50)
+                            : BorderRadius.circular(20),
+                      ),
+                      child: changeButton
+                          ? Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : Text(
+                              'Login',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                    ),
+                    onTap: () async {
+                      setState(() {
+                        changeButton = true;
+                      });
+                      await Future.delayed(Duration(milliseconds: 700));
                       Navigator.pushNamed(context, MyRoutes.homeRoute);
                     },
-                    child: Text(
-                      'Click Me',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                    style: TextButton.styleFrom(
-                        backgroundColor: Colors.purple[900],
-                        minimumSize: Size(120, 50)),
-                  )
+                  ),
                 ],
               ),
             ),
